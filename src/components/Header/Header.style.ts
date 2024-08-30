6c833723e7daafd32e6c274f103c71b794d5c874
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-08-30 00:04:35 -0300

    add header component simple

---
+++ src/components/Header/Header.style.ts
@@ -0,0 +1,9 @@
+import { Stack } from "@mui/material";
+import styled from "styled-components";
+
+export const HeaderStack = styled(Stack)(() => ({
+    background: 'ButtonFace',
+    padding: '5px', 
+    width: '100%', 
+    textAlign: 'center'
+}));
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import { HeaderStack } from './Header.style'
+import { Typography } from '@mui/material'
+
+export const Header = () => {
+    return (
+        <HeaderStack>
+            <Typography>
+                Bolos caseiros e veriedades
+            </Typography>
+        </HeaderStack>
+    )
+}
