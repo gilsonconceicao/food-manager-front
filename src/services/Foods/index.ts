ada8e70f087b4465577f1c4cbd289996b57a179e
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-09-30 23:14:18 -0300

    install tanstack library

--- src/services/Foods/index.ts
+++ src/services/Foods/index.ts
@@ -1,3 +1,8 @@
+import axios from "axios"
 import { BASE_URL } from "../api"
 
-const url = `${BASE_URL}/api/User`
+const url = `${BASE_URL}/Food`
+
+export const getAllFoods = () => {
+    return axios.get(`${url}`)
+}
--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import { User } from "./user.type"
 import { BASE_URL } from "../api"
 
-const url = `${BASE_URL}/api/User`
+const url = `${BASE_URL}/User`
 
 export const getUserByRegistrationNumberAsync = (registrationNumber: string) => {
     return axios.get<User>(`${url}/DocumentNumber/${registrationNumber}`)
