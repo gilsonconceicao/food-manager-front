5f6a82669b11a4308a6d47ad8c6b32debefc85f1
gilsonconceicao <gilsonsantosjunior02@gmail.com>
2024-10-04 00:03:20 -0300

    create column or row

--- src/Pages/Foods/Foods.tsx
+++ src/Pages/Foods/Foods.tsx
@@ -1,4 +1,5 @@
 import { FoodCard } from '@/components/FoodCard/FoodCard'
+import { useControlMenu } from '@/Hooks/Zustand/useMenuControl'
 import { ListPaginatation } from '@/services/@types/generic'
 import { Food } from '@/services/Foods/Foods.type'
 import { Stack } from '@mui/material'
@@ -9,8 +10,9 @@ type FoodsProps = {
 }
 
 export const Foods: React.FC<FoodsProps> = ({ queryData }) => {
+    const { isMobile } = useControlMenu();
     return (
-        <Stack direction='row' alignItems='center' flexWrap='wrap' spacing={2} gap={3}>
+        <Stack direction={isMobile ? 'column' : 'row'} alignItems='center' flexWrap='wrap' spacing={2} gap={3}>
             {queryData?.data.map((item, index) => {
                 return (
                     <FoodCard key={index} food={item}/>
